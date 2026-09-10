30b3b73f4e0a3d4bfbae78e718a878ef0ec64067
ramdaniokacha716-cell <ramdaniokacha716@gmail.com>
2026-09-11 01:25:49 +0200

    bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,16 @@ const qrcode = require('qrcode-terminal');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-       args: ['--no-sandbox', '--disable-setuid-sandbox']
+       args: [
+           '--no-sandbox', 
+           '--disable-setuid-sandbox',
+           '--disable-dev-shm-usage',
+           '--disable-accelerated-2d-canvas',
+           '--no-first-run',
+           '--no-zygote',
+           '--single-process',
+           '--disable-gpu'
+       ]
     }
 });
 
