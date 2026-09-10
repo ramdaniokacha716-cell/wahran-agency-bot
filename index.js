2c942e5f85dbdf79a22c5e7fb78c0b00ca524a30
ramdaniokacha716-cell <ramdaniokacha716@gmail.com>
2026-09-10 18:59:23 +0200

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
-تهيئة النموذج بالشكل الصحيح //
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 const cron = require('node-cron');
