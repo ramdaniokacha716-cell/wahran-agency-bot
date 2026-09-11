8083ff2d4d2ee8914ff231c79e4222f46e25e3b3
ramdaniokacha716-cell <ramdaniokacha716@gmail.com>
2026-09-11 17:26:23 +0200

    bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = service port , 8080;
+const PORT = process.env.PORT || 8080;
 let latestQR = '';
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
